5572284ca0e9f9177297e3577b71ce46205bf5c0
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2024-08-11 14:50:04 -0600

    don't load gtag unless it's active

--- public/analytics.js
+++ public/analytics.js
@@ -1,9 +1,14 @@
-(function () {
+(async function () {
   var SEND_ANALYTICS_FOR_NON_PROD = false;
 
   window.bitwiseCmdAnalyticsHandler = function () {
     return false;
   };
+
+  if (!!navigator.doNotTrack) {
+    console.log("Analytics tracking disabled by browser settings.");
+    return;
+  }
 
   var key = "TrackAnalytics";
   var disableAnalytics = localStorage.getItem(key) === "false" || window.location.hash.indexOf("-notrack") > -1;
@@ -14,18 +19,23 @@
     return;
   }
 
-  if (window.location.host !== "bitwisecmd.com" && !SEND_ANALYTICS_FOR_NON_PROD) {
-    console.log("Analytics not tracked. Non-prod host. SEND_ANALYTICS_FOR_NON_PROD=" + SEND_ANALYTICS_FOR_NON_PROD);
+  if (process.env.NODE_ENV === "production" && !SEND_ANALYTICS_FOR_NON_PROD) {
+    console.log("Analytics not tracked. Non-prod environment. SEND_ANALYTICS_FOR_NON_PROD=" + SEND_ANALYTICS_FOR_NON_PROD);
     return;
   }
 
-  window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments);
-  }
+  try {
+    await import("https://www.googletagmanager.com/gtag/js?id=G-H9EVNH8GNZ");
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
 
-  gtag("js", new Date());
-  gtag("config", "G-H9EVNH8GNZ");
+    gtag("js", new Date());
+    gtag("config", "G-H9EVNH8GNZ");
+  } catch (error) {
+    console.error("Failed to load gtag script:", error);
+  }
 
   window.bitwiseCmdAnalyticsHandler = function (evt) {
     gtag("event", evt.eventAction, evt);
